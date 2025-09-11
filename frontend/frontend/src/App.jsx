393fbaa037bac38099e1b059a481e88c908442d7
pradeepkumar673 <pradeepcastleofficial@gmail.comexit>
2025-09-11 16:14:12 +0530

    first

---
+++ frontend/frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './pages/login'
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+
+    </>
+  )
+}
+
+export default App
---
+++ frontend/frontend/src/pages/login.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+import lightLogo from '../assets/light.svg';
+import backgroundVideo from '../assets/324f53c8af273f72c36f5c0d35af7b30.mp4';
+
+const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Email:', email, 'Password:', password);
+    };
+
+    return (
+        <div className="relative min-h-screen flex flex-row justify-center items-center">
+            <video
+                autoPlay
+                loop
+                muted
+                className="absolute top-0 left-0 w-full h-full object-cover z-0"
+            >
+                <source src={backgroundVideo} type="video/mp4" />
+            </video>
+            <div className="absolute top-0 left-0 w-full h-full bg-black/45 z-0"></div>
+                <div className="relative z-10 flex flex-col justify-center items-center border rounded-md px-10 py-10 bg-black/40 border-none backdrop-blur-sm">
+                    <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                        <img src={lightLogo} alt="PradeepKumaeEmblem" className="mx-auto h-20 w-auto" />
+                        <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">Sign in to PradeepKumar</h2>
+                    </div>
+
+                    <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                        <form onSubmit={handleSubmit} className="space-y-6">
+                            <div>
+                                <label htmlFor="email" className="block text-sm/6 font-medium text-gray-100">Email address</label>
+                                <div className="mt-2">
+                                    <input
+                                        id="email"
+                                        type="email"
+                                        name="email"
+                                        required
+                                        autoComplete="email"
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                        className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                                    />
+                                </div>
+                            </div>
+
+                            <div>
+                                <div className="flex items-center justify-between">
+                                    <label htmlFor="password" className="block text-sm/6 font-medium text-gray-100">Password</label>
+                                    <div className="text-sm">
+                                        <a href="#" className="font-semibold text-indigo-400 hover:text-indigo-300">Forgot password?</a>
+                                    </div>
+                                </div>
+                                <div className="mt-2">
+                                    <input
+                                        id="password"
+                                        type="password"
+                                        name="password"
+                                        required
+                                        autoComplete="current-password"
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                        className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                                    />
+                                </div>
+                            </div>
+
+                            <div>
+                                <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Sign in</button>
+                            </div>
+                        </form>
+
+                        <p className="mt-10 text-center text-sm/6 text-gray-400">
+                            Not a member?
+                            <a href="#" className="font-semibold text-indigo-400 hover:text-indigo-300">Start a 14 day free trial</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        
+    )
+}
+
+export default Login
