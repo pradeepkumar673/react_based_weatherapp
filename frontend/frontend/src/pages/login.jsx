c35860882764a9c811f80d5ec3d09bbf6705e473
PradeepKumar673 <165313608+pradeepkumar673@users.noreply.github.com>
2025-09-12 17:22:05 +0530

    Update login.jsx

--- frontend/frontend/src/pages/login.jsx
+++ frontend/frontend/src/pages/login.jsx
@@ -28,7 +28,7 @@ const Login = () => {
         event.preventDefault();
 
         try {
-            const response = await fetch('http://localhost:5000/login', {
+            const response = await fetch('https://react-based-weatherapp-backend.onrender.com/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
